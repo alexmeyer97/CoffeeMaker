58bf57c1dd52de0c81d97f825ca79af4fc331840
Alexander Meyer <alexander@Alexanders-MacBook-Pro-2.local>
2018-06-01 02:44:30 -0700

    Now that's robust

--- me153project__2_.ino
+++ me153project__2_.ino
@@ -5,38 +5,35 @@ const char current = A0;    //pin speciffed by MOTORSHIELD to sense CURRENT
 const int pwm = 3;          //pin specified by MOTORSHIELD to control MOTORSPEED
 const int spd = 255 ;       //the set MOTORSPEED value
 
-const int lights = 11;      //pin specified by MOTORSHIELD to contorl MOTORSPEED2
+const int light = 11;      //pin specified by MOTORSHIELD to contorl MOTORSPEED2
 //repurposed to provide POWER to LIGHTS
 const int polarity = 13;    //pin specified by MOTORSHIELD to control MOTORDIRECTION2
 //repurposed to specifity POLARITY of LIGHTS
 
-const int floatSwitch = 2;      //pin connected to FLOATSWITCH
-const int closedSwitch = 5;     //pin connected to CLOSESWITCH
-const int openSwitch = 7;       //pin connected to OPENSWITCH
+const int floats = 2;      //pin connected to FLOATSWITCH
+const int closeds = 5;     //pin connected to CLOSESWITCH
+const int opens = 7;       //pin connected to OPENSWITCH
 
-const int overrideSwitch = 4;   //pin connected to OVERRIDESWITCH
-//TODO: change this to a better pin
-const int overrideLED = 1;      //pin connected to OVERRIDE LED
+const int overrides = 4;      //pin connected to OVERRIDESWITCH
+const int overrideLED = 1;    //pin connected to OVERRIDE LED
 
-const int sensor = 8;   //pin connected to IR sensor
+const int irsense = 8;   //pin connected to IR irsense
 
-bool floatUp;
-bool doorOpen;
-bool doorClosed;
+bool brewing;           //state variable that keeps track of RESERVOIR
+bool opened;            //state variable that keeps track of OPEN LIMIT SWITCH
+bool closed;            //state variable that keeps track of CLOSED LIMIT SWITCH
+bool overridePush;      //state variable that keeps track of OVERRIDESWITCH
+bool opening = false;   //state variable that keeps track of DOOR MOVEMENT towards OPEN position
+bool closing = false;   //state variable that keeps track of DOOR MOVEMENT towards CLOSED position
+bool brewed = true;     //state variable that keeps track of current coffee cycle
 
-//these variables are used to turn push button into a toggle
-bool overrideState = false;   //state of the override button right now
-bool lastState = false;       //state of the override button last iteration
-bool overrideEnabled = false; //state of the override
+//turns OVERRIDE button into toggle switch
+bool overridePush2 = false; //state variable that keeps track of OVERRIDESWITCH previous position
+bool overrideOn = false;    //current override state
 
-float dt = 10;
+unsigned long endTime;    //when coffee door can be opened after coffee is brewed
 
-unsigned long endTime;
-bool openStarted = false;
-
-
-void setup()
-{
+void setup() {
 
   pinMode(motor, OUTPUT);   //sets the MOTOR pin as output
   pinMode(brake, OUTPUT);   //sets the BRAKE pin as output
@@ -44,90 +41,82 @@ void setup()
   analogWrite(pwm, spd);    //sets the SPEED of the motor
   pinMode(current, INPUT);  //sets the CURRENT pin as input
 
-  pinMode(polarity, OUTPUT); //sets the POLARITY pin as output
-  pinMode(lights, OUTPUT);  //sets the LIGHTS pin as output
-  analogWrite(lights, spd); //sets the POWER provided to LIGHTS
+  pinMode(polarity, OUTPUT);  //sets the POLARITY pin as output
+  pinMode(light, OUTPUT);     //sets the LIGHTS pin as output
+  analogWrite(light, spd);    //sets the POWER provided to LIGHTS
 
-  pinMode(floatSwitch, INPUT_PULLUP);     //sets the FLOATSWITCH pin as input
-  pinMode(closedSwitch, INPUT_PULLUP);    //sets the CLOSESWITCH pin as input
-  pinMode(openSwitch, INPUT_PULLUP);      //sets the OPENSWITCH pin as input
-  pinMode(overrideSwitch, INPUT_PULLUP);  //sets the OVERRIDESWITCH pin as input
+  pinMode(floats, INPUT_PULLUP);     //sets the FLOATSWITCH pin as input
+  pinMode(closeds, INPUT_PULLUP);    //sets the CLOSESWITCH pin as input
+  pinMode(opens, INPUT_PULLUP);      //sets the OPENSWITCH pin as input
+  pinMode(overrides, INPUT_PULLUP);  //sets the OVERRIDESWITCH pin as input
 
-  pinMode(sensor, INPUT); //sets the IRSENSOR pin as input
+  pinMode(irsense, INPUT);  //sets the IRSENSOR pin as input
 
   pinMode(overrideLED, OUTPUT); //sets the OVERRIDE LED pin as output
 }
 
-void loop()
-{
+void loop() {
 
-  floatUp = digitalRead(floatSwitch) == HIGH;
-  doorClosed = digitalRead(closedSwitch) == LOW;
-  doorOpen = digitalRead(openSwitch) == LOW;
-  overrideState = digitalRead(overrideSwitch) == LOW;
+  brewing = digitalRead(floats) == HIGH;          //FLOATSWITCH up, water in reservoir, coffee brewing
+  closed = digitalRead(closeds) == LOW;           //CLOSESWITCH pressed, door fully closed
+  opened = digitalRead(opens) == LOW;             //OPENSWITCH pressed, door fully open
+  overridePush = digitalRead(overrides) == LOW;   //OVERRIDESWITCH pressed, check to toggle override mode
 
-  if (overrideState && (!lastState))
-  {
-    overrideEnabled = !overrideEnabled;
+  if (overridePush && (!overidePush2)) {
+    overrideOn = !overrideOn;
   }
-  lastState = overrideState;
+  overridePush2 = overridePush;
 
-  //motor movement stuff
-  if (!overrideEnabled)
-  {
-    if (floatUp && !doorClosed) //is there water in reservoir? and is the door not yet fully closed?
-    {
+  if (brewing && brewed) {
+    brewed = false;
+  }
+
+  if (!overrideOn) { //not in OVERRIDE state
+    if (brewing && !closing && !closed) { //FLOATSWITCH up, not MOVING towards CLOSED, and not CLOSED
       CloseDoor();
-    } else if ( (!floatUp) && (!doorOpen)) //is coffee done? and is the door not yet fully open?
-    {
-       if (!openStarted) //has the opening sequence not yet started?
-       {
-          if (doorClosed) // only begin opening sequence if the door has already closed fully
-          {
-           StopDoor();
-           openStarted = true; //start opening sequence
-           endTime = millis()+3000;
-          }
-       } else //has opening sequence started?
-       {  
-          if (millis()>endTime)//have we waited long enough
-          {
-          OpenDoor();
-          }
-       }
-    } else
-    {
-      openStarted = false;
+      closing = true;
+    } else if (brewing && closing && !closed) { //FLOATSWITCH up, MOVING towards CLOSED, and not CLOSED
+      while (!closed) {} end
+    } else if (!brewing && !opening && closed && !brewed) { // FLOATSWITCH down, not MOVING towards OPEN, CLOSED, and not brewed
+      opening = true;
+      endTime = millis() + 3000;
+    } else if (!brewing && opening && !opened) { //FLOATSWITCH down, MOVING towards OPEN, and not OPENED
+      brewed = true;
+      if (millis() > endTime) {
+        OpenDoor();
+      }
+    } else if (!brewing && !opening && opened) { //FLOATSWITCH down, not MOVING towards OPEN, and OPENED
+      endTime = millis() + 3000;
+      closing = true;
+    } else if (!brewing && !opening && !opened && !closed) { //FLOATSWITCH down, not MOVING towards OPEN, not OPENED, and not CLOSED
+      closing = true;
+      CloseDoor();
+    } else if (!brewing && closing && !closed) { //FLOATSWITCH down, MOVING towards CLOSED, and not CLOSED
+      if (millis() > endTime) {
+        CloseDoor();
+      }
+    } else {
+      opening = false;
+      closing = false;
       StopDoor();
-    }
-  } else
-  {
-    if (!doorOpen)
-    {
+    }  
+  } else {
+    if (!opened) {
       OpenDoor();
-      analogWrite(lights, spd);
+      analogWrite(light, spd);
       digitalWrite(polarity, HIGH);
-    }
-    else
-    {
+    } else {
       StopDoor();
-    }
+    }   
   }
 
-
-
-
-  //Lights up when door is completly open
-  if (doorOpen && (!floatUp))
-  {
-    analogWrite(lights, spd);
+  if (opened && !brewing) {
+    analogWrite(light, spd);
     digitalWrite(polarity, HIGH);
-  }
-  else
-  {
+  } else {
     digitalWrite(polarity, LOW);
-
   }
+  
 }
 
 void OpenDoor()
